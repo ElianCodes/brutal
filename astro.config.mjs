9232e8ca4d2f08fa58ceb3a30d58896cbb1df84b
Elian Van Cutsem <hello@elian.codes>
2023-04-22 20:43:33 +0200

    🛠️ remove outdated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,38 +7,14 @@ import uil from '@iconify-json/uil/icons.json';
 import presetWind from '@unocss/preset-wind';
 import presetTypography from '@unocss/preset-typography';
 
-const colors = [
-  'red',
-  'blue',
-  'green',
-  'yellow',
-  'pink',
-  'purple',
-  'orange',
-  'teal',
-  'cyan',
-  'lime',
-  'emerald',
-  'fuchsia',
-  'violet',
-  'rose',
-  'sky',
-  'amber',
-];
-
-const getSafeList = () => {
-  const list = [];
-  colors.forEach((color) => {
-    list.push(`hover:bg-${color}`);
-    list.push(`bg-${color}`);
-    list.push(`prose-${color}`);
-  });
-  return list;
-};
-
 export default defineConfig({
   // used to generate images
-  site: process.env.VERCEL_ENV === 'production' ? 'https://brutal.elian.codes/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3001/',
+  site:
+    process.env.VERCEL_ENV === 'production'
+      ? 'https://brutal.elian.codes/'
+      : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/`
+      : 'https://localhost:3001/',
   trailingSlash: 'ignore',
   integrations: [
     sitemap(),
@@ -53,7 +29,6 @@ export default defineConfig({
         }),
         presetTypography(),
       ],
-      safelist: getSafeList(),
     }),
   ],
 });
